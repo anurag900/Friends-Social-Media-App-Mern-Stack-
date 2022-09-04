ee8a4028adc5716570252404768d27d483a4e5f0
anurag900 <75412219+anurag900@users.noreply.github.com>
2022-09-04 18:38:50 +0530

    heroku deployable

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+
+const Footer = () => {
+
+  return (
+    <div className="footer">
+      <h3> Friends © 2022 || By - Anurag </h3>
+    </div>
+  );
+};
+
+export default Footer;
